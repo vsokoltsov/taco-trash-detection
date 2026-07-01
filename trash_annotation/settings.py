52f859ecdadd8b3cc28d0fb01164281f2bf7f76c
Vadim Sokoltsov <vforvad@gmail.com>
2026-07-01 21:31:56 +0200

    Add pydantic settings

---
+++ trash_annotation/settings.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from trash_annotation.storage import StorageEnum
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    MASK_RCNN_V1_PATH: str = ""
+    STORAGE: StorageEnum = StorageEnum.GOOGLE_DRIVE
+    USE_GPU: bool = False
+
+
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    return Settings()
